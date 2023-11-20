ae8ad8eb13d765223d53c37c6f7a6e50022de0bf
phuongle2kar3 <150886869+phuongle2kar3@users.noreply.github.com>
2023-11-20 22:34:32 +0700

    123

--- client-customer/src/components/HomeComponent.js
+++ client-customer/src/components/HomeComponent.js
@@ -42,9 +42,31 @@ class Home extends Component {
         {this.state.hotprods.length > 0 ?
           <div className="align-center">
             <h2 className="text-center">HOT PRODUCTS</h2>
+            
             {hotprods}
           </div>
           : <div />}
+        <div className='align-bottom'>
+          <div id='suport'>
+            <div id='detail1'>
+              TỔNG ĐÀI HỖ TRỢ (MIỄN PHÍ)
+            </div>
+            <div id='detail2'>
+              Gọi mua: 1800.1234 (8:00 - 21:00)<br/>
+              CSKH:    1800.5678 (8:00 - 21:00)<br/>
+              TTBH:    1800.6565 (8:00 - 21:00)
+            </div>
+              
+          
+          </div>
+          <div id='info'>KẾT NỐI VỚI CHÚNG TÔI
+            <Link to='https://www.facebook.com/lhp231103/'><img src='facebook.png' alt="Logo"></img></Link>
+            <Link to='https://www.facebook.com/lhp231103/'><img src='instagram.png' alt="Logo"></img></Link>
+            <Link to='https://www.facebook.com/lhp231103/'><img src='tik-tok.png' alt="Logo"></img></Link>
+            <Link to='https://www.facebook.com/lhp231103/'><img src='youtube.png' alt="Logo"></img></Link>
+            <Link to='https://www.facebook.com/lhp231103/'><img src='gmail.png' alt="Logo"></img></Link>
+          </div>
+        </div>
       </div>
     );
   }
--- client-customer/src/components/InformComponent.js
+++ client-customer/src/components/InformComponent.js
@@ -9,13 +9,13 @@ class Inform extends Component {
       <div className="border-bottom">
         <div className="float-left">
         {this.context.token === '' ?
-          <div id='account'><Link to='/login'>Login</Link> | <Link to='/signup'>Sign-up</Link> | <Link to='/active'>Active</Link></div>
+          <div id='account'><Link to='/login'><img src='login.png' alt="Logo" />Login</Link>  <Link to='/signup'><img src='sign-up.png' alt="Logo" />Sign-up</Link>  <Link to='/active'><img src='active.png' alt="Logo" />Active</Link></div>
           :
-          <div>Hello <b>{this.context.customer.name}</b> | <Link to='/home' onClick={() => this.lnkLogoutClick()}>Logout</Link> | <Link to='/myprofile'>My profile</Link> |  <Link to='/myorders'>My orders</Link></div>
+          <div>Xin chào ! <b>{this.context.customer.name}</b><div id='logout'> <Link to='/home' onClick={() => this.lnkLogoutClick()}>Logout</Link> <Link to='/myprofile'>My profile</Link> <Link to='/myorders'>My orders</Link></div></div>
         }
         </div>
         <div className="float-right">
-        <Link to='/mycart'>My cart</Link> have <b>{this.context.mycart.length}</b> items
+        <div id='mycart'><Link to='/mycart'><img src='mycart.png' alt="Logo" /></Link><b>{this.context.mycart.length}</b></div>
         
         </div>
         <div className="float-clear" />
